df62a79e2622bfc55e61c3bcbed2e194bc249051
Lakshman Ekambaram <ekambara@us.ibm.com>
2021-05-28 19:44:59 -0500

    Create registry-cleanup.sh

---
+++ registry-cleanup.sh
@@ -0,0 +1,43 @@
+!/bin/bash
+
+# based on: https://gist.github.com/jaytaylor/86d5efaddda926a25fa68c263830dac1
+# changes: 
+# - iterate over tag list instead of using only first one
+# - added basic auth
+# - fixed http header to 'docker-content-digest'
+
+
+# exit when any command fails
+set -e
+PORTABLE_REGISTRY_USER=admin
+PORTABLE_REGISTRY_PASS=admin
+PORTABLE_REGISTRY_URL='localhost:5000'
+
+# Get all images
+images=`curl -k -u $PORTABLE_REGISTRY_USER:$PORTABLE_REGISTRY_PASS https://${PORTABLE_REGISTRY_URL}/v2/_catalog?n=10000 | jq .repositories[]`
+
+
+for image in $images; do
+    echo "DELETING: " $image
+
+    # get tag list of image
+    tags=$(curl --user $PORTABLE_REGISTRY_USER:$PORTABLE_REGISTRY_PASS "https://${registry}/v2/${image}/tags/list" | jq -r '.tags // [] | .[]' | tr '\n' ' ')
+
+    # check for empty tag list
+    if [[ -n $tags ]]
+    then
+        for tag in $tags; do
+            curl --user $PORTABLE_REGISTRY_USER:$PORTABLE_REGISTRY_PASS -X DELETE "https://${PORTABLE_REGISTRY_URL}/v2/${image}/manifests/$(
+                curl --user $PORTABLE_REGISTRY_USER:$PORTABLE_REGISTRY_PASS -I \
+                    -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+                    "https://${PORTABLE_REGISTRY_URL}/v2/${image}/manifests/${tag}" \
+                | awk '$1 == "docker-content-digest:" { print $2 }' \
+                | tr -d $'\r' \
+            )"
+        done
+
+        echo "DONE:" $image
+    else
+        echo "SKIP:" $image
+    fi
+done
